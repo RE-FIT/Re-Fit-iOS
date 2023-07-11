b6f7e34f5d86732a1fa975cbb1f2435cfa684163
JaeKyu Park <pjk971113@naver.com>
2023-07-12 01:01:24 +1200

    [Feat] 아이디/비밀번호 찾기 UI 완성

---
+++ RE-FIT/Login/FindTabViewController.swift
@@ -0,0 +1,80 @@
+//
+//  FindTabViewController.swift
+//  RE-FIT
+//
+//  Created by jaegu park on 11/07/23.
+//
+
+import UIKit
+import Tabman
+import Pageboy
+
+class FindTabViewController: TabmanViewController {
+    
+    @IBOutlet weak var TabView: UIView!
+    
+    private var viewControllers: Array<UIViewController> = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabMan()
+    }
+        
+    private func setupTabMan(){
+        let vc1 = UIStoryboard.init(name: "Login", bundle: nil).instantiateViewController(withIdentifier: "TabIDVC") as! TabIDViewController
+        let vc2 = UIStoryboard.init(name: "Login", bundle: nil).instantiateViewController(withIdentifier: "TabPWVC") as! TabPWViewController
+        
+        viewControllers.append(vc1)
+        viewControllers.append(vc2)
+        
+        self.dataSource = self
+        
+        let bar = TMBar.ButtonBar()
+        // 배경 회색으로 나옴 -> 하얀색으로 바뀜
+        bar.backgroundView.style = .clear
+        // 간격 설정
+        bar.layout.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 25, right: 0)
+        // 버튼 글씨 커스텀
+        bar.buttons.customize { (button) in
+            button.tintColor = .systemGray4
+            button.selectedTintColor = UIColor(red: 36/255, green: 117/255, blue: 53/255, alpha: 1)
+            button.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+            button.selectedFont = UIFont.systemFont(ofSize: 16, weight: .bold)
+        }
+
+        
+        bar.layout.contentMode = .fit
+        
+        // 밑줄 쳐지는 부분
+        bar.indicator.weight = .custom(value: 4)
+        bar.indicator.tintColor = UIColor(red: 36/255, green: 117/255, blue: 53/255, alpha: 1)
+        addBar(bar, dataSource: self, at: .custom(view: TabView, layout: nil))
+    }
+}
+
+extension FindTabViewController: PageboyViewControllerDataSource, TMBarDataSource {
+    
+    func barItem(for bar: TMBar, at index: Int) -> TMBarItemable {
+        switch index {
+        case 0:
+            return TMBarItem(title: "아이디 찾기")
+        case 1:
+            return TMBarItem(title: "비밀번호 찾기")
+        default:
+            let title = "Page \(index)"
+           return TMBarItem(title: title)
+        }
+    }
+
+    func numberOfViewControllers(in pageboyViewController: PageboyViewController) -> Int {
+        return viewControllers.count
+    }
+    
+    func viewController(for pageboyViewController: PageboyViewController, at index: PageboyViewController.PageIndex) -> UIViewController? {
+        return viewControllers[index]
+    }
+    
+    func defaultPage(for pageboyViewController: PageboyViewController) -> PageboyViewController.Page? {
+        return nil
+    }
+}
