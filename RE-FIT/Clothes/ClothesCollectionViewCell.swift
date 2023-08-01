521450c1e4b5f4e5f1ed009cff736fc69064fe33
JaeKyu Park <pjk971113@naver.com>
2023-08-02 00:21:08 +1200

    [Feat] 커뮤니티 UI 수정 및 옷장 CollectionView UI 구현 완료

---
+++ RE-FIT/Clothes/ClothesCollectionViewCell.swift
@@ -0,0 +1,66 @@
+//
+//  ClothesCollectionViewCell.swift
+//  RE-FIT
+//
+//  Created by jaegu park on 1/08/23.
+//
+
+import UIKit
+
+class ClothesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var DayView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        contentView.layer.cornerRadius = 15
+        contentView.layer.masksToBounds = true
+        
+        layer.cornerRadius = 15
+        layer.masksToBounds = false
+        
+        DayView.layer.cornerRadius = 15
+        DayView.layer.masksToBounds = false
+        DayView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMaxYCorner]
+    }
+    
+}
+
+extension ClothesViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let width = collectionView.frame.width
+//        let height = collectionView.frame.height
+//        let itemsPerRow: CGFloat = 2
+//        let widthPadding = sectionInsets.left * (itemsPerRow + 1)
+//        let itemsPerColumn: CGFloat = 3
+//        let heightPadding = sectionInsets.top * (itemsPerColumn + 1)
+//        let cellWidth = (width - widthPadding) / itemsPerRow
+//        let cellHeight = (height - heightPadding) / itemsPerColumn
+        
+        return CGSize(width: 168, height: 145)
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+        
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: ClothesCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Clothes_CollectionViewCell", for: indexPath) as! ClothesCollectionViewCell
+        
+        return cell
+    }
+}
--- RE-FIT/Clothes/ClothesViewController.swift
+++ RE-FIT/Clothes/ClothesViewController.swift
@@ -6,24 +6,126 @@
 //
 
 import UIKit
+import DropDown
 
 class ClothesViewController: UIViewController {
-
+    
+    @IBOutlet weak var TopView: UIView!
+    @IBOutlet weak var BottomView: UIView!
+    @IBOutlet weak var OuterView: UIView!
+    @IBOutlet weak var OnepieceView: UIView!
+    @IBOutlet weak var ShoeView: UIView!
+    @IBOutlet weak var TopButton: UIButton!
+    @IBOutlet weak var BottomButton: UIButton!
+    @IBOutlet weak var OuterButton: UIButton!
+    @IBOutlet weak var OnepieceButton: UIButton!
+    @IBOutlet weak var ShoeButton: UIButton!
+    @IBOutlet weak var TopLabel: UILabel!
+    @IBOutlet weak var BottomLabel: UILabel!
+    @IBOutlet weak var OuterLabel: UILabel!
+    @IBOutlet weak var OnepieceLabel: UILabel!
+    @IBOutlet weak var ShoeLabel: UILabel!
+    @IBOutlet weak var SeasonTextField: UITextField!
+    @IBOutlet weak var SortTextField: UITextField!
+    @IBOutlet weak var ClothesCV: UICollectionView!
+    
+    
+    let dropdownSeason = DropDown()
+    let dropdownSort = DropDown()
+    
+    let Season = [" 봄 • 가을", " 여름", " 겨울"]
+    let Sort = [" D-Day 순", " 많이 입은 순", " 적게 입은 순"]
+    
+    let sectionInsets = UIEdgeInsets(top: 0, left: 18, bottom: 18, right: 18)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        SeasonTextField.layer.cornerRadius = 10
+        SeasonTextField.layer.borderColor = UIColor.white.cgColor
+        SeasonTextField.layer.borderWidth = 1.0
+        SeasonTextField.layer.masksToBounds = true
+        SortTextField.layer.cornerRadius = 10
+        SortTextField.layer.borderColor = UIColor.white.cgColor
+        SortTextField.layer.borderWidth = 1.0
+        SortTextField.layer.masksToBounds = true
+        
+        self.ClothesCV.delegate = self
+        self.ClothesCV.dataSource = self
 
-        // Do any additional setup after loading the view.
+        initUISeason()
+        initUISort()
+        setDropdownSeason()
+        setDropdownSort()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func initUISeason() {
+        DropDown.appearance().textColor = UIColor.white // 아이템 텍스트 색상
+        DropDown.appearance().selectedTextColor = UIColor.black // 선택된 아이템 텍스트 색상
+        DropDown.appearance().backgroundColor = UIColor(red: 36/255, green: 117/255, blue: 53/255, alpha: 1) // 아이템 팝업 배경 색상
+        DropDown.appearance().selectionBackgroundColor = UIColor(red: 36/255, green: 117/255, blue: 53/255, alpha: 1) // 선택한 아이템 배경 색상
+        DropDown.appearance().setupCornerRadius(10)
+        dropdownSeason.dismissMode = .automatic // 팝업을 닫을 모드 설정
+        DropDown.appearance().textFont = UIFont(name: "AppleSDGothicNeo-Medium", size: 11) ?? UIFont.systemFont(ofSize: 11)
     }
-    */
-
+    
+    func initUISort() {
+        DropDown.appearance().textColor = UIColor.white // 아이템 텍스트 색상
+        DropDown.appearance().selectedTextColor = UIColor.black // 선택된 아이템 텍스트 색상
+        DropDown.appearance().backgroundColor = UIColor(red: 36/255, green: 117/255, blue: 53/255, alpha: 1) // 아이템 팝업 배경 색상
+        DropDown.appearance().selectionBackgroundColor = UIColor(red: 36/255, green: 117/255, blue: 53/255, alpha: 1) // 선택한 아이템 배경 색상
+        DropDown.appearance().setupCornerRadius(10)
+        dropdownSort.dismissMode = .automatic // 팝업을 닫을 모드 설정
+        DropDown.appearance().textFont = UIFont(name: "AppleSDGothicNeo-Medium", size: 11) ?? UIFont.systemFont(ofSize: 11)
+    }
+    
+    func setDropdownSeason() {
+        // dataSource로 ItemList를 연결
+        dropdownSeason.dataSource = Season
+        dropdownSeason.cellHeight = 27
+        // anchorView를 통해 UI와 연결
+        dropdownSeason.anchorView = self.SeasonTextField
+        
+        // View를 갖리지 않고 View아래에 Item 팝업이 붙도록 설정
+        dropdownSeason.bottomOffset = CGPoint(x: 0, y: SeasonTextField.bounds.height)
+        
+        // Item 선택 시 처리
+        dropdownSeason.selectionAction = { [weak self] (index, item) in
+            //선택한 Item을 TextField에 넣어준다.
+            self!.SeasonTextField.text = " \(item)"
+        }
+        
+        // 취소 시 처리
+        dropdownSeason.cancelAction = { [weak self] in
+        }
+    }
+    
+    func setDropdownSort() {
+        // dataSource로 ItemList를 연결
+        dropdownSort.dataSource = Sort
+        dropdownSort.cellHeight = 27
+        // anchorView를 통해 UI와 연결
+        dropdownSort.anchorView = self.SortTextField
+        
+        // View를 갖리지 않고 View아래에 Item 팝업이 붙도록 설정
+        dropdownSort.bottomOffset = CGPoint(x: 0, y: SortTextField.bounds.height)
+        
+        // Item 선택 시 처리
+        dropdownSort.selectionAction = { [weak self] (index, item) in
+            //선택한 Item을 TextField에 넣어준다.
+            self!.SortTextField.text = " \(item)"
+        }
+        
+        // 취소 시 처리
+        dropdownSort.cancelAction = { [weak self] in
+        }
+    }
+    
+    @IBAction func Season_Tapped(_ sender: Any) {
+        dropdownSeason.show()
+    }
+    
+    @IBAction func Sort_Tapped(_ sender: Any) {
+        dropdownSort.show()
+    }
 }
